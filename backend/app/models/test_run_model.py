1bc559c7cbb1869f4f804511dd200a5581b9e555
Aishwarya A Poojary <aishwaryaapoojary51@gmail.com>
2026-03-26 21:17:06 +0530

    Update test_run_model.py

--- backend/app/models/test_run_model.py
+++ backend/app/models/test_run_model.py
@@ -18,12 +18,12 @@ class TestRun(Base):
     status = Column(String(50))  # Running / Passed / Failed
     coverage = Column(Integer)
 
-    # ✅ Execution tracking
+    #  Execution tracking
     duration = Column(String(50))
     started_at = Column(DateTime, default=datetime.utcnow)
     completed_at = Column(DateTime)
 
-    # ✅ Logs (store JSON string)
+    #  Logs (store JSON string)
     logs = Column(Text)
 
     created_at = Column(DateTime, default=datetime.utcnow)
